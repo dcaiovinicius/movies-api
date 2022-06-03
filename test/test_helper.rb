262b1c05a23aef39479c19f67fb88a8c6f282700
user01 <caioviniciusdev@gmail.com>
2022-06-03 18:07:17 -0400

    Use simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,13 +3,17 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'simplecov'
+
+# Load and launch SimpleCov
+SimpleCov.start
 
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
-    # Setting up syntax and methods to FactoryBot
+    # Configuring syntax and methods for FactoryBot
     include FactoryBot::Syntax::Methods
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
