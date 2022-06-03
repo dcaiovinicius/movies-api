791b0a90a6a8dd120190a225cbf08bc556f8642a
user01 <caioviniciusdev@gmail.com>
2022-06-03 17:57:09 -0400

    Remove published_at attribute

--- test/factories/movie.rb
+++ test/factories/movie.rb
@@ -6,7 +6,6 @@ FactoryBot.define do
     genre { 'TV Show' }
     country { 'United States' }
     year {  Date.strptime('2001', '%y').year }
-    published_at { DateTime.current }
     description { 'A classmate receives a series of tapes that unravel the mystery of her tragic choice.' }
   end
 end
