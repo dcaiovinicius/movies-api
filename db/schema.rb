285192bbbaf7afb9167201abddc4fe9a6aa04a30
user01 <caioviniciusdev@gmail.com>
2022-06-03 21:30:18 -0400

    Adding migrations to slug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_03_180750) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_03_221436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_180750) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug", null: false
   end
 
 end
