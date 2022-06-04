48066f9e3bc31bb8112d5dae65abb748ff771645
user01 <caioviniciusdev@gmail.com>
2022-06-03 22:01:24 -0400

    Adding validation for year

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,9 +4,10 @@ class Movie < ApplicationRecord
   validates :title, presence: true, length: { maximum: 500 }
   validates :genre, presence: true, length: { maximum: 600 }
   validates :country, presence: true, length: { maximum: 20 }
-  validates :year, presence: true
   validates :published_at, presence: true
   validates :description, presence: true
+
+  validates :year, presence: true, format: { with: /(19|20)\d{2}/i, message: I18n.t('movie.four-digit') }
   validates :slug, presence: true, uniqueness: { scope: :title, message: I18n.t('movie.registered_error') }
 
   before_validation :set_published_at
