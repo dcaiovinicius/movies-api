d099ddd55713ca6fbc0bb9af96250965466c7fc7
user01 <caioviniciusdev@gmail.com>
2022-06-03 21:26:43 -0400

    Adding validations to slug

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,12 +7,20 @@ class Movie < ApplicationRecord
   validates :year, presence: true
   validates :published_at, presence: true
   validates :description, presence: true
+  validates :slug, presence: true, uniqueness: { scope: :title, message: I18n.t('movie.registered_error') }
 
   before_validation :set_published_at
+  before_validation :set_slug
 
   private
 
   def set_published_at
     self.published_at = DateTime.current
+  end
+
+  def set_slug
+    return slug if title.blank?
+
+    self.slug = title.parameterize
   end
 end
