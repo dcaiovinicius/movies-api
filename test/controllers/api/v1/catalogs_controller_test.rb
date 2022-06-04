8c5990854b9f784454d0fbf5c524155938df67a2
user01 <caioviniciusdev@gmail.com>
2022-06-04 16:34:42 -0400

    Test the endpoint that returns a movie

--- test/controllers/api/v1/catalogs_controller_test.rb
+++ test/controllers/api/v1/catalogs_controller_test.rb
@@ -5,8 +5,17 @@ require 'test_helper'
 module Api
   module V1
     class CatalogsControllerTest < ActionDispatch::IntegrationTest
-      test 'GET /api/v1/movies' do
+      setup do
+        @movie = create(:movie)
+      end
+
+      test 'should return all movies' do
         get api_v1_catalogs_url
+        assert_response :ok
+      end
+
+      test 'should only show one movie' do
+        get api_v1_catalog_url(@movie)
         assert_response :ok
       end
     end
