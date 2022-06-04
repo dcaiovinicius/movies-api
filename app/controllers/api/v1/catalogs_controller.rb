08085cd7e608efa63ae181e455afe19dc05f3f19
user01 <caioviniciusdev@gmail.com>
2022-06-04 18:37:51 -0400

    Adding endpoint to create a new movie

--- app/controllers/api/v1/catalogs_controller.rb
+++ app/controllers/api/v1/catalogs_controller.rb
@@ -3,10 +3,33 @@
 module Api
   module V1
     class CatalogsController < ApiController
-      # GET /api/v1/movies
+      # GET /api/v1/catalogs
       def index
         @movies = Movies::Query.call(options: params)
-        render json: @movies, only: %i[title genre country year published_at]
+        render json: @movies, only: %i[id title genre country year published_at]
+      end
+
+      # GET /api/v1/catalogs/uuid
+      def show
+        @movie = Movie.find(params[:id])
+        render json: @movie, only: %i[id title genre country year published_at]
+      end
+
+      # POST /api/v1/catalogs
+      def create
+        @movie = Movie.new(movie_params)
+
+        if @movie.save
+          render json: { message: 'Saved movie' }, status: :ok
+        else
+          render json: { status: 'ERROR', errors: @movie.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def movie_params
+        params.require(:movie).permit(:title, :genre, :country, :year, :published_at, :description)
       end
     end
   end
