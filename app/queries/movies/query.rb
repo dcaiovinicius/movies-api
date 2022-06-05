d5a3ab1d4d5ab5e3238dcb70eee06e1e70e821e3
user01 <caioviniciusdev@gmail.com>
2022-06-05 13:15:22 -0400

    Sort movie by year

--- app/queries/movies/query.rb
+++ app/queries/movies/query.rb
@@ -2,33 +2,22 @@
 
 module Movies
   class Query
-    # ALLOWED_PARAMS = %i[year title].freeze
-
     def self.call(relation: Movie, options: {})
-      return relation.all if options.values_at(:year, :title, :country, :published_at).all?(nil)
+      year, title, genre, country, published_at = options.values_at(:year, :title, :genre, :country, :published_at)
 
-      relation = search_by_year(relation, options[:year]) if options[:year]
-      relation = search_by_title(relation, options[:title]) if options[:title]
-      relation = search_by_country(relation, options[:country]) if options[:country]
-      relation = search_by_published_at(relation, options[:published_at]) if options[:published_at]
-
-      relation
-    end
-
-    def self.search_by_published_at(relation, published_at)
-      relation.where(published_at: published_at)
-    end
-
-    def self.search_by_country(relation, country)
-      relation.where(country: country)
-    end
-
-    def self.search_by_year(relation, year)
-      relation.where(year: year)
-    end
-
-    def self.search_by_title(relation, title)
-      relation.where('lower(title) ILIKE ?', "%#{title}%")
+      if year
+        relation.where(year: year)
+      elsif title
+        relation.where('lower(title) ILIKE ?', "%#{title}%").order(year: :desc)
+      elsif country
+        relation.where(country: country).order(year: :desc)
+      elsif published_at
+        relation.where(published_at: published_at).order(year: :desc)
+      elsif genre
+        relation.where(genre: genre).order(year: :desc)
+      else
+        relation.order(year: :desc)
+      end
     end
   end
 end
